5b2b9740100593049cc2ffc93e829690ad549588
Jeff Kleinaitis <jeffkleinaitis@gmail.com>
2023-08-12 23:28:01 -0400

    Updated the way user objects were handled and displayed.
    This commit changes the way that user objects were displayed throughout the application. Previously, methods such as  formatForCreation,  formatForListing, etc., were created to be used to format users in a specific type of way. This was removed to better handle passing data to different parts of the application. The application now uses getters methods to gather information about specific attributes.

--- gridspace-sift-dashboard/src/App.js
+++ gridspace-sift-dashboard/src/App.js
@@ -1,26 +1,100 @@
 import React, { useState, useEffect } from 'react';
 import { fetchAllUsers } from './services/fetch-data';
 import UserTable from './components/UserTable';
-import ControlButtons from './components/ControlButtons';
+import UpdateUser from './components/UpdateUser';
 
 function App() {
-  const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState(null);
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [userToUpdate, setUserToUpdate] = useState(null);
+    const [uniqueRoles, setUniqueRoles] = useState([]);
 
     useEffect(() => {
-        async function getUsers() {
+        async function getUsersAndRoles() {
             const fetchedUsers = await fetchAllUsers();
-            setUsers(fetchedUsers);
+            const usersById = fetchedUsers.reduce((acc, user) => {
+                acc[user.userId] = user;
+                return acc;
+            }, {});
+            setUsers(usersById);
+
+            // Extract unique roles from the user objects
+            const allRoles = [...new Set(Object.values(usersById).map(user => user.role))];
+            setUniqueRoles(allRoles);
         }
 
-        getUsers();
+        getUsersAndRoles();
     }, []);
 
-  return (
-      <div>
-        <ControlButtons />
-        <UserTable users={users} />
-      </div>
-  );
+    const onCreateUser = () => {
+        // Logic for creating a new user
+    };
+
+    const handleUserSelect = (userId) => {
+        setSelectedUser(userId);
+    };
+
+    const handleUserListRefresh = async () => {
+        // Fetch updated user data and refresh the user list
+        const fetchedUsers = await fetchAllUsers();
+        const usersById = fetchedUsers.reduce((acc, user) => {
+            acc[user.userId] = user;
+            return acc;
+        }, {});
+        setUsers(usersById);
+    };
+
+    const onUpdateUser = async () => {
+        if (selectedUser) {
+            const user = users[selectedUser];
+            setUserToUpdate(user);
+            setModalOpen(true);
+        }
+    };
+
+    const handleUpdateSuccess = async (updatedUserData) => {
+        try {
+            const updatedUsers = await fetchAllUsers();
+            const usersById = updatedUsers.reduce((acc, user) => {
+                acc[user.userId] = user;
+                return acc;
+            }, {});
+            setUsers(usersById);
+
+            // Reset selected user
+            setSelectedUser(null);
+
+            // Close the modal
+            setModalOpen(false);
+        } catch (error) {
+            console.error("Failed to fetch updated user data:", error);
+        }
+    };
+    return (
+        <div className="App">
+            <div className="App-header">
+                <h1>User Management</h1>
+            </div>
+            <div className="table-container">
+                <UserTable users={Object.values(users)} onUserSelect={handleUserSelect} selectedUserId={selectedUser} />
+                <div className="action-buttons">
+                    <button className="button button-create" onClick={onCreateUser}>Create New User</button>
+                    <button className="button" onClick={onUpdateUser} disabled={!selectedUser}>Update Existing User</button>
+                </div>
+            </div>
+            {isModalOpen && (
+                <UpdateUser
+                    isOpen={isModalOpen}
+                    onClose={() => setModalOpen(false)}
+                    user={userToUpdate}
+                    onUpdateSuccess={handleUpdateSuccess}
+                    roles={uniqueRoles}
+                    onUserListRefresh={handleUserListRefresh}
+                />
+            )}
+        </div>
+    );
 }
 
 export default App;
--- gridspace-sift-dashboard/src/models/UserClass.js
+++ gridspace-sift-dashboard/src/models/UserClass.js
@@ -1,58 +1,82 @@
 export default class User {
+    get data() {
+        return this._data;
+    }
+
+    get userId() {
+        return this._userId;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    get team() {
+        return this._team;
+    }
+
+    get role() {
+        return this._role;
+    }
+
+    get orgNode() {
+        return this._orgNode;
+    }
+
+    get firstName() {
+        return this._firstName;
+    }
+
+    get lastName() {
+        return this._lastName;
+    }
+
+    get agentId() {
+        return this._agentId;
+    }
+
+    get externalId() {
+        return this._externalId;
+    }
+
+    get throttle() {
+        return this._throttle;
+    }
+
+    get displayName() {
+        return this._displayName;
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    get lastLogin() {
+        return this._lastLogin;
+    }
     constructor(data) {
-        this.userId = data.user_id || null; // only set when updating or after creation
-        this.name = data.name || null;
-        this.email = data.email || null;
-        this.team = data.team || null; // for batch creation
-        this.role = data.role || null;
+        this._userId = data.user_id || null; // only set when updating or after creation
+        this._name = data._name || null;
+        this._email = data.email || null;
+        this._team = data._team || null; // for batch creation
+        this._role = data.role || null;
 
         // attributes from the update API
-        this.orgNode = data.org_node || null;
-        this.firstName = data.first_name || null;
-        this.lastName = data.last_name || null;
-        this.agentId = data.agent_id || null;
-        this.externalId = data.external_id || null;
-        this.throttle = data.throttle || null;
+        this._orgNode = data.org_node ? { id: data.org_node.id, name: data.org_node.name } : null;
+        this._firstName = data.first_name || null;
+        this._lastName = data.last_name || null;
+        this._agentId = data.agent_id || null;
+        this._externalId = data.external_id || null;
+        this._throttle = data._throttle || null;
 
         // attributes from the list users response, not necessary for creation/update but might be useful
-        this.displayName = data.display_name || null;
-        this.status = data.status || null;
-        this.lastLogin = data.last_login || null;
-    }
-
-    formatForListing() {
-        return {
-            UserID: this.userId,
-            DisplayName: this.displayName,
-            Email: this.email,
-            Status: this.status,
-            LastLogin: this.lastLogin,
-            OrgNode: this.orgNode?.name || "",
-            Name: this.name,
-            FirstName: this.firstName,
-            LastName: this.lastName,
-            ExternalID: this.externalId
-        };
-    }
-
-    formatForCreation() {
-        return {
-            name: this.name,
-            email: this.email,
-            team: this.team,
-            role: this.role
-        };
-    }
-
-    formatForUpdate() {
-        return {
-            org_node: this.orgNode,
-            role: this.role,
-            first_name: this.firstName,
-            last_name: this.lastName,
-            agent_id: this.agentId,
-            external_id: this.externalId,
-            throttle: this.throttle
-        };
+        this._displayName = data.display_name || null;
+        this._status = data.status || null;
+        this._lastLogin = data.last_login || null;
+        this._data = data;
     }
 }
