a9305e0e3893532487591fb6ebd345df5533ea85
Jeff Kleinaitis <jeffkleinaitis@gmail.com>
2023-08-12 23:23:57 -0400

    Updated UI elements.
    This commit updates a few UI elements across various files. It completely removes the ControlButton.js class and updates the fields displayed for users in the table and modifies the rows accordingly. Also changed elements within the UserTable.css file to make the application look more professionally styled.

--- gridspace-sift-dashboard/src/components/UserRow.js
+++ gridspace-sift-dashboard/src/components/UserRow.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import './UserTable.css';
 
-function UserRow({ user }) {
+function UserRow({ user, onRowClick, isSelected }) {
+    function formatDate(lastLogin) {
+        if (!lastLogin) {
+            return 'Never'; // Handles lastLogin missing
+        }
+
+        const localTime = new Date(lastLogin);
+
+        const options = {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+        };
+
+        return new Intl.DateTimeFormat(undefined, options).format(localTime);
+    }
+
     return (
-        <tr>
-            <td>{user.UserID}</td>
-            <td>{user.DisplayName}</td>
-            <td>{user.Email}</td>
-            <td>{user.Status}</td>
-            <td>{user.LastLogin}</td>
-            <td>{user.OrgNode}</td>
-            <td>{user.Name}</td>
-            <td>{user.FirstName}</td>
-            <td>{user.LastName}</td>
-            <td>{user.ExternalID}</td>
+        <tr onClick={() => onRowClick(user)} className={isSelected ? 'selected-row' : ''}>
+            <td>{user.firstName}</td>
+            <td>{user.lastName}</td>
+            <td>{user.email}</td>
+            <td>{user.status}</td>
+            <td>{formatDate(user.lastLogin)}</td>
+            <td>{user.orgNode ? user.orgNode.name : ''}</td>
         </tr>
     );
 }
--- gridspace-sift-dashboard/src/components/UserTable.js
+++ gridspace-sift-dashboard/src/components/UserTable.js
@@ -1,41 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import { fetchAllUsers } from '../api/fetch-data';
+import React, { useState } from 'react';
 import UserRow from './UserRow';
+import './UserTable.css';
 
-function UserTable() {
-    const [users, setUsers] = useState([]);
+function UserTable({ users, onUserSelect }) {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [usersPerPage, setUsersPerPage] = useState(5);
+    const [selectedUser, setSelectedUser] = useState(null);
 
-    useEffect(() => {
-        async function loadUsers() {
-            const fetchedUsers = await fetchAllUsers();
-            setUsers(fetchedUsers);
-        }
+    const lastIndex = currentPage * usersPerPage;
+    const firstIndex = lastIndex - usersPerPage;
+    const currentUsers = users.slice(firstIndex, lastIndex);
 
-        loadUsers();
-    }, []);
+    const handleRowClick = (user) => {
+        setSelectedUser(user);
+        onUserSelect(user.userId);
+    };
+
+    const onPageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const onUsersPerPageChange = (event) => {
+        setUsersPerPage(event.target.value);
+    };
 
     return (
-        <table>
-            <thead>
-            <tr>
-                <th>User ID</th>
-                <th>Display Name</th>
-                <th>Email</th>
-                <th>Status</th>
-                <th>Last Login</th>
-                <th>Org Node</th>
-                <th>Name</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>External ID</th>
-            </tr>
-            </thead>
-            <tbody>
-            {users.map(user => (
-                <UserRow key={user.UserID} user={user} />
-            ))}
-            </tbody>
-        </table>
+        <div className="user-table-container">
+            <div className="users-per-page-container">
+                <label>Users per page: </label>
+                <select className="users-per-page-select" onChange={onUsersPerPageChange} value={usersPerPage}>
+                    <option value="5">5</option>
+                    <option value="10">10</option>
+                    <option value="15">15</option>
+                </select>
+            </div>
+            <table className="user-table">
+                <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Status</th>
+                    <th>Last Login</th>
+                    <th>Org Node</th>
+                </tr>
+                </thead>
+                <tbody>
+                {currentUsers.map(user => (
+                    <UserRow
+                        key={user.userId}
+                        user={user}
+                        onRowClick={handleRowClick}
+                        isSelected={user === selectedUser}
+                    />
+                ))}
+                </tbody>
+            </table>
+            <div className="button-container">
+                <button className="button" onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+                <button className="button" onClick={() => onPageChange(currentPage + 1)} disabled={currentPage * usersPerPage >= users.length}>Next</button>
+            </div>
+        </div>
     );
 }
 
