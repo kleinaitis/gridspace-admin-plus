ecd4deddd2bc44a951fc3ff17bd20c309f5b0c99
Jeff Kleinaitis <jeffkleinaitis@gmail.com>
2023-08-13 22:52:14 -0400

    Added login screen for the application.
    This commit adds a login screen for the application. Rather than the account ID and the secret key being passed in the program like it was in previous commits, it is now input by the user. Upon a successful API request, the user is greeted with the table of users, otherwise, an error occurs.

---
+++ gridspace-sift-dashboard/src/components/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { fetchAllUsers } from '../services/fetch-data';
+import './Login.css';
+
+function Login(props) {
+    const [accountId, setAccountId] = useState('');
+    const [secretKey, setSecretKey] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLogin = async () => {
+        const credentials = window.btoa(accountId + ":" + secretKey);
+
+        try {
+            const users = await fetchAllUsers(credentials);
+            if (users.error) {
+                setError('Login failed!');
+            } else {
+                props.onLoginSuccess(users, credentials);
+            }
+        } catch (error) {
+            setError('An error occurred while logging in.');
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <input type="text" placeholder="Account ID" onChange={e => setAccountId(e.target.value)} />
+            <input type="password" placeholder="Secret Key" onChange={e => setSecretKey(e.target.value)} />
+            <button onClick={handleLogin}>Login</button>
+            {error && <div className="error-message">{error}</div>}
+        </div>
+    );
+}
+
+export default Login;
