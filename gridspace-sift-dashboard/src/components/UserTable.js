e7f6dfa69569497e828aa56c18c81059314bd46a
Jeff Kleinaitis <jeffkleinaitis@gmail.com>
2023-08-14 20:39:20 -0400

    Added search functionality to the user table, and modified interface.
    This commit adds the functionality to search the user table by a user's full name, first name, last name, or email address. The user table automatically updates as keystrokes are captured.

--- gridspace-sift-dashboard/src/components/UserTable.js
+++ gridspace-sift-dashboard/src/components/UserTable.js
@@ -6,10 +6,21 @@ function UserTable({ users, onUserSelect }) {
     const [currentPage, setCurrentPage] = useState(1);
     const [usersPerPage, setUsersPerPage] = useState(5);
     const [selectedUser, setSelectedUser] = useState(null);
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const filteredUsers = users.filter(user => {
+        const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
+        return (
+            fullName.includes(searchQuery.toLowerCase()) ||
+            user.firstName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.lastName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            user.email.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+    });
 
     const lastIndex = currentPage * usersPerPage;
     const firstIndex = lastIndex - usersPerPage;
-    const currentUsers = users.slice(firstIndex, lastIndex);
+    const currentUsers = filteredUsers.slice(firstIndex, lastIndex);
 
     const handleRowClick = (user) => {
         setSelectedUser(user);
@@ -24,15 +35,31 @@ function UserTable({ users, onUserSelect }) {
         setUsersPerPage(event.target.value);
     };
 
+    const onSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+        setCurrentPage(1);
+    };
+
     return (
         <div className="user-table-container">
             <div className="users-per-page-container">
-                <label>Users per page: </label>
-                <select className="users-per-page-select" onChange={onUsersPerPageChange} value={usersPerPage}>
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="15">15</option>
-                </select>
+                <div className="users-per-page-text">
+                    <span>View: </span>
+                    <select className="users-per-page-select" onChange={onUsersPerPageChange} value={usersPerPage}>
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="15">15</option>
+                    </select>
+                </div>
+                <div className="search-bar-container">
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className="search-bar"
+                        value={searchQuery}
+                        onChange={onSearchChange}
+                    />
+                </div>
             </div>
             <table className="user-table">
                 <thead>
@@ -57,8 +84,12 @@ function UserTable({ users, onUserSelect }) {
                 </tbody>
             </table>
             <div className="button-container">
-                <button className="button" onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
-                <button className="button" onClick={() => onPageChange(currentPage + 1)} disabled={currentPage * usersPerPage >= users.length}>Next</button>
+                <button className="icon-button" onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
+                    <i className="fas fa-chevron-left"></i>
+                </button>
+                <button className="icon-button" onClick={() => onPageChange(currentPage + 1)} disabled={currentPage * usersPerPage >= users.length}>
+                    <i className="fas fa-chevron-right"></i>
+                </button>
             </div>
         </div>
     );
