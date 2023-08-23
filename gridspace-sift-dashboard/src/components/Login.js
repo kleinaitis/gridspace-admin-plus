6754b30c52e2195a9c0936dd9e3b6c3e3088426e
Jeff Kleinaitis <jeffkleinaitis@gmail.com>
2023-08-22 21:38:50 -0400

    Adjusted styling of login section, and added text to the body of the Need Help button.

--- gridspace-sift-dashboard/src/components/Login.js
+++ gridspace-sift-dashboard/src/components/Login.js
@@ -17,6 +17,7 @@ function Login(props) {
             const users = await fetchAllUsers(credentials);
             if (users.error) {
                 setError('Login failed!');
+                setTimeout(() => setError(''), 5000); // Clear the error after 5 seconds
             } else {
                 props.onLoginSuccess(users, credentials);
             }
@@ -25,6 +26,7 @@ function Login(props) {
             setError('An error occurred while logging in.');
         }
     };
+
 
     const openHelpDialog = () => {
         setHelpDialogOpen(true);
@@ -38,7 +40,10 @@ function Login(props) {
         <div className="login-page">
             <div className="login-container">
                 <div className="login-header">
-                    <h1>Login</h1>
+                    <h1>Sign In</h1>
+                </div>
+                <div className="login-header-description">
+                    <h1>Use your Account ID and Secret Key from Gridspace.</h1>
                 </div>
                 <div className="account-id-container">
                     <input
@@ -58,21 +63,29 @@ function Login(props) {
                 </div>
                 <div className="button-container">
                     <button className="login-button" onClick={handleLogin}>
-                        Login
+                        Sign In
                     </button>
-                    <div className="help-button-container">
-                        <button className="help-button" onClick={openHelpDialog}>
-                            <FontAwesomeIcon icon={faQuestionCircle} />
-                        </button>
-                    </div>
+                    <button className="back-button" onClick={props.handleBack}>
+                        Back
+                    </button>
                 </div>
                 {error && <div className="input-error">{error}</div>}
+                <div className="help-button-container">
+                    <button className="help-button" onClick={openHelpDialog}>
+                        Need Help?
+                    </button>
+                </div>
             </div>
             {helpDialogOpen && (
                 <div className="help-dialog-overlay">
                     <div className="help-dialog">
                         <h2>Need Help?</h2>
-                        <p>If you're experiencing any issues....</p>
+                        <p>To find your Account ID and Secret Key:</p>
+                        <ol>
+                            <li>Visit <a href="https://api.gridspace.com/account" target="_blank" rel="noopener noreferrer">https://api.gridspace.com/account</a></li>
+                            <li>Log in to your Gridspace account</li>
+                            <li>Locate your Account ID and Secret Key on the page</li>
+                        </ol>
                         <button className="close-button" onClick={closeHelpDialog}>Close</button>
                     </div>
                 </div>
