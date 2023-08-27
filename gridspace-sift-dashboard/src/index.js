945d4c192ff0170d7006bfc30f1144cf8eaf7cce
Jeff Kleinaitis <jeffkleinaitis@gmail.com>
2023-08-27 09:59:36 -0400

    Updated to use createRoot API to resolve "ReactDOM render no longer supported in React 18," error mesage.

--- gridspace-sift-dashboard/src/index.js
+++ gridspace-sift-dashboard/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from "./App";
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 reportWebVitals();
